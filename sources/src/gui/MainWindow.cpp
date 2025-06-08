7d4088a441be06569be20f3fe9b734d02ac650c7
Hesiodion <francoisfadel@gmail.com>
2025-06-08 18:12:19 +0200

    PACE : Adding Qt environment and create the main window sample

---
+++ sources/src/gui/MainWindow.cpp
@@ -0,0 +1,16 @@
+#include "ui_MainWindow.h"
+
+#include "MainWindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , m_ui(new Ui::MainWindow)
+{
+    m_ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete m_ui;
+}
